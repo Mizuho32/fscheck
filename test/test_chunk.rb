aeaeff618a233bc963d27d32421dede193493bb2
Mizuho <dummy32@hotmail.co.jp>
2017-01-22 13:06:54 +0900

    superblockchunk test

--- test/test_chunk.rb
+++ test/test_chunk.rb
@@ -41,12 +41,70 @@ class Chunk_Test < Test::Unit::TestCase
     ch0 = FileSystem::SuperBlockChunk.new(block: Chunk_Test.bl0, index: 0)
     ch1 = FileSystem::SuperBlockChunk.new(block: Chunk_Test.bl0, index: 1)
 
-    assert_equal(0x1F1E1D1C, ch1.size)
-    ch1.size = 1
+    assigs = [
+      0x0000FF00,
+      0x0000A0A0,
+      0x0000D00D,
+      0xAAAA0000,
+      0xAAAA1111,
+      0xAAAA2222,
+      0xAAAA3333,
 
+      0x12340000,
+      0x0000CCCC,
+      0x10100000,
+      0x33001100,
+      0xFF00FF00,
+      0xABCDEF00,
+      0x22002222
+    ]
     assert_equal(0x03020100, ch0.size)
-    ch0.size = 0x0000FF00
-    p Chunk_Test.fs.image
+    ch0.size        = assigs[0]
+
+    assert_equal(0x07060504, ch0.nblocks)
+    ch0.nblocks     = assigs[1]
+
+    assert_equal(0x0B0A0908, ch0.ninodes)
+    ch0.ninodes     = assigs[2]
+
+    assert_equal(0x0F0E0D0C, ch0.nlog)
+    ch0.nlog        = assigs[3]
+
+    assert_equal(0x13121110, ch0.logstart)
+    ch0.logstart    = assigs[4]
+
+    assert_equal(0x17161514, ch0.inodestart)
+    ch0.inodestart  = assigs[5]
+
+    assert_equal(0x1B1A1918, ch0.bmapstart)
+    ch0.bmapstart   = assigs[6]
+
+
+    assert_equal(0x1F1E1D1C, ch1.size)
+    ch1.size        = assigs[7]
+
+    assert_equal(0x23222120, ch1.nblocks)
+    ch1.nblocks     = assigs[8]
+
+    assert_equal(0x27262524, ch1.ninodes)
+    ch1.ninodes     = assigs[9]
+
+    assert_equal(0x2B2A2928, ch1.nlog)
+    ch1.nlog        = assigs[10]
+
+    assert_equal(0x2F2E2D2C, ch1.logstart)
+    ch1.logstart    = assigs[11]
+
+    assert_equal(0x33323130, ch1.inodestart)
+    ch1.inodestart  = assigs[12]
+
+    assert_equal(0x37363534, ch1.bmapstart)
+    ch1.bmapstart   = assigs[13]
+
+
+    expeted_image = assigs.pack("I14")
+    assert_equal(expeted_image, Chunk_Test.fs.image)
+    #p "", Chunk_Test.fs.image[0...28]
   end
 
 end
