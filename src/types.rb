167c80b8293a3ffcc8c92688dc665d175c89a657
Mizuho <dummy32@hotmail.co.jp>
2017-02-06 02:48:23 +0900

    changed types.rb for more general case

--- src/types.rb
+++ src/types.rb
@@ -14,7 +14,7 @@ end
 
 class FileSystem
 
-  attr_accessor :image, :block_size, :blocks, :size_block
+  attr_accessor :image, :block_size, :blocks, :size_block, :chunked_block
 
   def initialize(image: nil, block_size: 512)
     @image = image
@@ -28,9 +28,21 @@ class FileSystem
     fs.size_block = definition
 
     fs.blocks[ definition[:superblock] ] = FileSystem::ChunkedBlock.new(fs: fs, index: definition[:superblock].first, klass: FileSystem::SuperBlockChunk)
-    definition[:inodeblock].each{|i|
+
+    sb = fs.superblock[0][0]
+    irange = fs.size_block[:inodeblock] = if ind = definition[:inodeblock] then
+      ind
+    else
+      (sb.inodestart..sb.inodestart+sb.ninodes/8)
+    end
+    irange.each{|i|
       fs.blocks[i] = FileSystem::ChunkedBlock.new(fs: fs, index: i, klass: FileSystem::DinodeBlockChunk)
     }
+
+    bsize = sb.size/(512*8)+1
+    definition[:bitmapblock] = (sb.bmapstart..sb.bmapstart+bsize-1)
+
+    fs.chunked_block = fs.size_block.select{|b,r| b == :superblock || b == :inodeblock}
 
     fs
   end
@@ -45,8 +57,9 @@ class FileSystem
 
   def inodeblocks
     inodeblk = inodeblock()
+    sb = superblock()[0][0]
     @inodeblocks = @inodeblocks || Enumerator.new{|chunk|
-      (0...26).each do |block_index|
+      (0...sb.ninodes/8+1).each do |block_index|
         (0...8).each { |inode_index|
           chunk << inodeblk[block_index][inode_index]
         }
@@ -55,11 +68,13 @@ class FileSystem
   end
 
   def bitmapblock
-    bitmap = self.[](58)
+    bitmaps = self.[](@size_block[:bitmapblock])
     @bitmapblock = @bitmapblock || Enumerator.new{|bit|
-      (0...512).each do |byte_index|
-        (0...8).each{|bit_shift|
-          bit << ( (bitmap[byte_index]>>bit_shift)&1 )
+      bitmaps.each do |bitmap|
+        (0...512).each { |byte_index|
+          (0...8).each do |bit_shift|
+            bit << ( (bitmap[byte_index]>>bit_shift)&1 )
+          end
         }
       end
     }
@@ -69,8 +84,9 @@ class FileSystem
     cur = @blocks[i]
 
     if Range === i then
+      cur = [nil] if cur.nil?
       any_nil = cur.any?(&:nil?)
-      raise Exception.new("Unable to access chunked block") if(@size_block.values.any?{|r| r.include?(i.max) || r.include?(i.min) } && any_nil)
+      raise Exception.new("Unable to access chunked block") if(@chunked_block.values.any?{|r| r.include?(i.max) || r.include?(i.min) } && any_nil)
       if any_nil then
         cur.each_with_index.each{|el, cur_i| @blocks[idx=(cur_i + i.first)] = FileSystem::Block.new(fs: self, index: idx) if el.nil?
         }
