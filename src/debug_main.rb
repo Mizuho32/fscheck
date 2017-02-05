19204a62e65361c6be8dc74d8819c15998ffd62f
Mizuho <dummy32@hotmail.co.jp>
2017-02-06 02:47:35 +0900

    change debug_main xv6_load parameter

--- src/debug_main.rb
+++ src/debug_main.rb
@@ -3,7 +3,7 @@ require 'test/unit/ui/console/testrunner'
 load 'types.rb'
 load 'maintest.rb'
 
-$fs = FileSystem.load_xv6_fs(fsimg: $image, block_size: 512, definition: {superblock: 1..1, inodeblock: 32..57})
+$fs = FileSystem.load_xv6_fs(fsimg: $image, block_size: 512, definition: {superblock: 1..1})
 
 Init.init()
 Test::Unit::AutoRunner.run
